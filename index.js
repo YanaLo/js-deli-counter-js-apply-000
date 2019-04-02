ca3611065a5d647998f37cb7e9fb334445a63752
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-04-02 20:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(theLine){
   }
   else{
     for(let i =0; i<theLine.length;i++){
-      staticStmt+=`${i+1}. ${theLine[i]}${theLine.length-1===i}`
+      staticStmt+=`${i+1}. ${theLine[i]}${theLine.length-1===i?", ":""}`
     }
   }
 }
